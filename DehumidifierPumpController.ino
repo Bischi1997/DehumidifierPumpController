068d47833c30e9713b64a7f52051c55cab1de425
Bischi1997 <rafael.bischofberger@gmail.com>
2021-07-13 23:28:57 +0200

    updated DehumidifierPumpController

--- DehumidifierPumpController.ino
+++ DehumidifierPumpController.ino
@@ -1,36 +1,35 @@
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;// the number of the pushbutton pin
+const int waterSensorPin = 2;// the number of the water sensor pin
 const int ledPin =  13; // the number of the LED pin
-const int relay = 7;    // for Arduino microcontroller
+const int relay = 7;    // for pump relay
 
 
-// variables will change:
-int buttonState = 0;    // variable for reading the pushbutton status
+// variables that will change:
+int waterSensorState = 0;    // variable for reading the water sensor status
 
 void setup() {
-  Serial.begin(9600);
-  
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(waterSensorPin, INPUT);
   // initialize the relay pin:
   pinMode(relay, OUTPUT);
 }
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  //Serial.println(buttonState);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  waterSensorState = digitalRead(waterSensorPin);
+  // check if the pushbutton is pressed. If it is, the waterSensorState is HIGH:
+  if (waterSensorState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
+    // turn Pump on for 102s:
     digitalWrite(relay, HIGH);
     delay(102000);
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
+    // turn Pump off:
     digitalWrite(relay, LOW);
   }
 }
